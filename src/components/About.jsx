de158ad68e306039d01a97358b3ca3495b015c6e
omkushwaha9 <omkushwaha2005@gmail.com>
2024-07-30 22:17:25 +0530

    About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 function About() {
   return (
-    <div>
-      
+    <div className='w-full p-16 bg-[#CDEA68] rounded-tl-xl rounded-tr-xl text-black'>
+      <h1 className='font-["Neue_Montreal"] text-5xl'>
+      Ochi is a strategic partner for fast-grow­ing tech businesses that need to raise funds,
+       sell prod­ucts, ex­plain com­plex ideas, and hire great peo­ple.
+      </h1>
     </div>
   )
 }
