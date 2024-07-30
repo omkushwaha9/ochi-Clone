59d15f49436f28d57310ba5d24b633f0820b9fbe
omkushwaha9 <omkushwaha2005@gmail.com>
2024-07-30 18:03:38 +0530

    Landing Page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 
 function LandingPage() {
-  return (
-    <div className='w-full h-screen bg-zinc-950 pt-1'>
-      <div className='textstructure mt-32 px-16'>
-        <div className='masker'>
-            <h1 className=' uppercase text-9xl leading-none tracking-tight font-["Founders_Grotesk"] '>We Create</h1>
-
+    return (
+        <div className='w-full h-screen bg-zinc-950 pt-1'>
+            <div className='textstructure mt-32 px-16'>
+                {["We Create", "Eye opening", "Presentations"].map((item,index)=>{
+                    return <div className='masker'>
+                       <h1 className='uppercase text-[7vw] leading-[7vw] tracking-tighter font-semibold '>
+                        {item}
+                       </h1>
+                    </div>
+                })}
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default LandingPage
