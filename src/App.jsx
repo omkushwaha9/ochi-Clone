5f5af79881b7079d815da3ff22d78bedd6a53a50
omkushwaha9 <omkushwaha2005@gmail.com>
2024-08-04 19:04:41 +0530

    Locomotive scroll

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,12 @@ import Eyes from './components/Eyes'
 import Featured from './components/Featured'
 import Cards from './components/Cards'
 import Footer from './components/Footer'
+import LocomotiveScroll from 'locomotive-scroll';
 
 function App() {
+
+const locomotiveScroll = new LocomotiveScroll();
+
   return (
     <div className='w-full min-h-screen bg-zinc-950 text-white'>
       <Navbar />
